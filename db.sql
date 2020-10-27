d8483d24640ed1c22509f1bc9360e3695e147bc5
LucyMerlier <lucymerlier@gmail.com>
2020-10-27 11:01:34 +0100

    ajout de quelques questions/réponses dans la bdd via db.sql

--- db.sql
+++ db.sql
@@ -7,3 +7,43 @@ CREATE TABLE author ( id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, name VARCHAR(1
 CREATE TABLE theme ( id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, title VARCHAR(20) NOT NULL);
 
 ALTER TABLE choice ADD CONSTRAINT fk_choice_question FOREIGN KEY (question_id) REFERENCES question(id);
+
+INSERT INTO question (question) VALUES ("Les chaussettes de l'archiduchesse sont-elles sèches?");
+
+INSERT INTO choice (answer, validity, question_id) VALUES ("Oui" , TRUE , 1);
+
+INSERT INTO choice (answer, validity, question_id) VALUES ("Archi-sèches" , TRUE , 1);
+
+INSERT INTO choice (answer, validity, question_id) VALUES ("Non" , FALSE , 1);
+
+INSERT INTO question (question) VALUES ("Pourquoi?");
+
+INSERT INTO choice (answer, validity, question_id) VALUES ("Parce-que" , TRUE , 2);
+
+INSERT INTO choice (answer, validity, question_id) VALUES ("42" , TRUE , 2);
+
+INSERT INTO choice (answer, validity, question_id) VALUES ("Je sais pô..." , FALSE , 2);
+
+INSERT INTO question (question) VALUES ("0+0=?");
+
+INSERT INTO choice (answer, validity, question_id) VALUES ("0" , TRUE , 3);
+
+INSERT INTO choice (answer, validity, question_id) VALUES ("La tête à Toto" , TRUE , 3);
+
+INSERT INTO choice (answer, validity, question_id) VALUES ("78" , FALSE , 3);
+
+INSERT INTO choice (answer, validity, question_id) VALUES ("False" , TRUE , 3);
+
+INSERT INTO question (question) VALUES ("Quelle est la capitale du Venezuela?");
+
+INSERT INTO choice (answer, validity, question_id) VALUES ("Paris" , FALSE , 4);
+
+INSERT INTO choice (answer, validity, question_id) VALUES ("Londres" , FALSE , 4);
+
+INSERT INTO choice (answer, validity, question_id) VALUES ("Pékin" , FALSE , 4);
+
+INSERT INTO choice (answer, validity, question_id) VALUES ("Caracas" , TRUE , 4);
+
+INSERT INTO choice (answer, validity, question_id) VALUES ("Montevideo" , FALSE , 4);
+
+INSERT INTO choice (answer, validity, question_id) VALUES ("Moscou" , FALSE , 4);
