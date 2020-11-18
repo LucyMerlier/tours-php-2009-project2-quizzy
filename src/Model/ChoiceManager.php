ffb99797858a11885889766a7b9b75b6a22ee1ee
LucyMerlier <lucymerlier@gmail.com>
2020-11-18 13:57:48 +0100

    created choices controller that creates the page that allows users to add their choices and select their validities and the page that tells the user if their question/choices have successfully been added to database, and created method in choicemanager for complexity issues

--- src/Model/ChoiceManager.php
+++ src/Model/ChoiceManager.php
@@ -10,6 +10,9 @@ class ChoiceManager extends AbstractManager
 {
     public const TABLE = 'choice';
     public const DATABASE_ERROR = -1;
+    public const CHOICE_MAX_LENGTH = 100;
+    public const CHOICE_MIN_LENGTH = 1;
+    public const MIN_NUMBER_OF_VALID_CHOICES = 1;
 
     /**
      *  Initializes this class.
@@ -54,5 +57,45 @@ class ChoiceManager extends AbstractManager
         }
 
         return self::DATABASE_ERROR;
+    }
+
+    /**
+     * Choices and validity verification
+     * @param array $userChoices
+     * @return array
+     */
+    public function choicesVerifications(array $userChoices): array
+    {
+        $errors = [];
+        $numberOfValidChoices = 0;
+
+        // Check if each choice isn't too long or too short
+        foreach ($userChoices as $userChoice) {
+            if (strlen($userChoice['answer']) > self::CHOICE_MAX_LENGTH) {
+                if (empty($errors[0])) {
+                    $errors[0] = "L'un de tes choix est trop long! Ils doivent faire maximum "
+                    . self::CHOICE_MAX_LENGTH . " caractères.";
+                }
+            }
+
+            if (strlen($userChoice['answer']) < self::CHOICE_MIN_LENGTH) {
+                if (empty($errors[1])) {
+                    $errors[1] = "L'un de tes choix est trop court! Ils doivent faire minimum "
+                    . self::CHOICE_MIN_LENGTH . " caractère.";
+                }
+            }
+
+            // Count the valid choices
+            if ($userChoice['validity'] === '1') {
+                $numberOfValidChoices++;
+            }
+        }
+
+        // Check if there is at least one good choice
+        if ($numberOfValidChoices < self::MIN_NUMBER_OF_VALID_CHOICES) {
+            $errors[2] = "Il te faut au minimum " . self::MIN_NUMBER_OF_VALID_CHOICES . " bonne réponse!";
+        }
+
+        return $errors;
     }
 }
